fc1e2f3667f25de6364705840a7fc87f141cba2b
Carl Hembrough <carl@bitwaves.co.uk>
2017-11-20 15:37:57 +0000

    Extract methods from the Handle method.

--- handler/incoming_instance_handler.go
+++ handler/incoming_instance_handler.go
@@ -39,6 +39,12 @@ type DimensionRepository interface {
 	Close()
 }
 
+// ObservationRepository provides storage for observations.
+type ObservationRepository interface {
+	CreateConstraint(instance *model.Instance) error
+	Close()
+}
+
 // CompletedProducer Producer kafka messages for instances that have been successfully processed.
 type CompletedProducer interface {
 	Completed(e event.InstanceCompleted) error
@@ -48,14 +54,13 @@ type CompletedProducer interface {
 type InstanceEventHandler struct {
 	NewDimensionInserter  func() (DimensionRepository, error)
 	NewInstanceRepository func() (InstanceRepository, error)
+	NewObservationRepository func() (ObservationRepository, error)
 	DatasetAPICli         DatasetAPIClient
 	Producer              CompletedProducer
 }
 
-// Handle retrieves the dimensions for specified instanceID from the Import API, creates an MyInstance entity for
-// provided instanceID, creates a Dimension entity for each dimension and a relationship to the MyInstance it belongs to
-// and makes a PUT request to the Import API with the database ID of each Dimension entity.
-func (hdlr *InstanceEventHandler) Handle(newInstance event.NewInstance) error {
+func (hdlr *InstanceEventHandler) validate(newInstance event.NewInstance) error {
+
 	if hdlr.DatasetAPICli == nil {
 		return errors.New(" validation error dataset api client required but was nil")
 	}
@@ -69,19 +74,31 @@ func (hdlr *InstanceEventHandler) Handle(newInstance event.NewInstance) error {
 		return errors.New(" validation error instance_id required but was empty")
 	}
 
+	return nil
+}
+
+// Handle retrieves the dimensions for specified instanceID from the Import API, creates an MyInstance entity for
+// provided instanceID, creates a Dimension entity for each dimension and a relationship to the MyInstance it belongs to
+// and makes a PUT request to the Import API with the database ID of each Dimension entity.
+func (hdlr *InstanceEventHandler) Handle(newInstance event.NewInstance) error {
+
+	err := hdlr.validate(newInstance)
+	if err != nil {
+		return err
+	}
+
 	logData := log.Data{
 		"instance_id": newInstance.InstanceID,
 	}
 	logger.Info("handling new instance event", logData)
 
 	start := time.Now()
+
 	var dimensions []*model.Dimension
-	var err error
 
 	if dimensions, err = hdlr.DatasetAPICli.GetDimensions(newInstance.InstanceID); err != nil {
 		return errors.Wrap(err, "DatasetAPICli.GetDimensions returned an error")
 	}
-
 	logData["dimensions_count"] = len(dimensions)
 
 	// retrieve the CSV header from the dataset API and attach it to the instance node allowing it to be used after import.
@@ -96,7 +113,60 @@ func (hdlr *InstanceEventHandler) Handle(newInstance event.NewInstance) error {
 	}
 	defer instanceRepo.Close()
 
+	err = hdlr.createInstanceNode(instance, instanceRepo)
+	if err != nil {
+		return err
+	}
+
+	err = hdlr.insertDimensions(instance, instanceRepo, dimensions)
+	if err != nil {
+		return err
+	}
+
+	instanceProcessed := event.InstanceCompleted{
+		FileURL:    newInstance.FileURL,
+		InstanceID: newInstance.InstanceID,
+	}
+
+	if err := hdlr.Producer.Completed(instanceProcessed); err != nil {
+		return errors.Wrap(err, "Producer.Completed returned an error")
+	}
+
+	logger.Info("instance processing completed successfully", log.Data{"processing_time": time.Since(start).Seconds()})
+	return nil
+}
+
+
+func (hdlr *InstanceEventHandler) insertDimensions(instance *model.Instance, instanceRepo InstanceRepository, dimensions []*model.Dimension) (error) {
+
+	dimensionInserter, err := hdlr.NewDimensionInserter()
+	if err != nil {
+		return errors.Wrap(err, "NewDimensionInserter returned an error")
+	}
+	defer dimensionInserter.Close()
+
+	for _, dimension := range dimensions {
+		if dimension, err = dimensionInserter.Insert(instance, dimension); err != nil {
+			return errors.Wrap(err, "error while attempting to insert dimension")
+		}
+
+		if err = hdlr.DatasetAPICli.PutDimensionNodeID(instance.InstanceID, dimension); err != nil {
+			return errors.Wrap(err, "DatasetAPICli.PutDimensionNodeID returned an error")
+		}
+	}
+
+	if err = instanceRepo.AddDimensions(instance); err != nil {
+		return errors.Wrap(err, "instanceRepo.AddDimensions returned an error")
+	}
+
+	return nil
+}
+
+func (hdlr *InstanceEventHandler) createInstanceNode(instance *model.Instance, instanceRepo InstanceRepository) error {
+
 	var exists bool
+	var err error
+
 	if exists, err = instanceRepo.Exists(instance); err != nil {
 		return errors.Wrap(err, "instance repository exists check returned an error")
 	}
@@ -113,35 +183,6 @@ func (hdlr *InstanceEventHandler) Handle(newInstance event.NewInstance) error {
 		return errors.Wrap(err, "instanceRepo.Create returned an error")
 	}
 
-	dimensionInserter, err := hdlr.NewDimensionInserter()
-	if err != nil {
-		return errors.Wrap(err, "NewDimensionInserter returned an error")
-	}
-	defer dimensionInserter.Close()
-
-	for _, dimension := range dimensions {
-		if dimension, err = dimensionInserter.Insert(instance, dimension); err != nil {
-			return errors.Wrap(err, "error while attempting to insert dimension")
-		}
-
-		if err = hdlr.DatasetAPICli.PutDimensionNodeID(newInstance.InstanceID, dimension); err != nil {
-			return errors.Wrap(err, "DatasetAPICli.PutDimensionNodeID returned an error")
-		}
-	}
-
-	if err = instanceRepo.AddDimensions(instance); err != nil {
-		return errors.Wrap(err, "instanceRepo.AddDimensions returned an error")
-	}
-
-	instanceProcessed := event.InstanceCompleted{
-		FileURL:    newInstance.FileURL,
-		InstanceID: newInstance.InstanceID,
-	}
-
-	if err := hdlr.Producer.Completed(instanceProcessed); err != nil {
-		return errors.Wrap(err, "Producer.Completed returned an error")
-	}
-
-	logger.Info("instance processing completed successfully", log.Data{"processing_time": time.Since(start).Seconds()})
 	return nil
 }
+
